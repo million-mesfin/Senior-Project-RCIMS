a1077ff5e12f99adf98de55f21e8b4bf66cee8f8
Million Mesfin <millionmesfind@gmail.com>
2024-09-02 13:00:23 -0300

    Modified for creating professionals

--- backend/src/controller/Signup.js
+++ backend/src/controller/Signup.js
@@ -31,15 +31,16 @@ async function signupUser(req, res) {
         });
 
         const savedUser = await newUser.save();
-        res.status(201).json({
-            message: "User created successfully",
-            user: savedUser,
-        });
+        // res.status(201).json({
+        //     message: "User created successfully",
+        //     user: savedUser,
+        // });
+        return savedUser;
     } catch (error) {
         console.log(error);
         console.log("Internal server error");
         res.status(400).json({
-            error: "Unable to create user. Please try again later."
+            error: "Unable to create user. Please try again later.",
         });
     }
 }
