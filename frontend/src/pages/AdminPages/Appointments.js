e793ebc16f23df153d9f74163309f8cbc3e7ec99
Million Mesfin <millionmesfind@gmail.com>
2024-09-02 13:06:40 -0300

    Added componenets for the Admin dashboard

---
+++ frontend/src/pages/AdminPages/Appointments.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Appointments = () => {
+
+    return (
+        <div>
+            <h1>Appointments</h1>
+            <p>This is the appointments page.</p>
+        </div>
+    );
+};
+
+export default Appointments;
+            
+    
---
+++ frontend/src/pages/AdminPages/PatientManagement.js
@@ -0,0 +1,14 @@
+import React from "react";
+const PatientManagement = () => {   
+
+    return (
+        <div>
+            <h1>Patient Management</h1>
+            <p>This is the patient management page.</p>
+        </div>
+    );
+};
+
+export default PatientManagement;
+            
+    
