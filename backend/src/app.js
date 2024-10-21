97982a9ad69fec79d6ee372b91ec401fc11d0fe6
Million Mesfin <millionmesfind@gmail.com>
2024-10-20 23:26:57 -0300

    Add feedback management functionality with routes and controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,7 @@ const {
     updateAppointmentStatus,
 } = require("./controller/AppointmentController");
 const appointmentManagementRoute = require("./routes/AppointmentManagement");
+const feedbackRoute = require("./routes/Feedback");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -76,6 +77,7 @@ app.use("/api/caregiver", caregiverManagementRoute);
 app.use("/api/patient-history", patientHistoryManagementRoute);
 app.use("/api/schedule", scheduleRoute);
 app.use("/api/appointment", appointmentManagementRoute);
+app.use("/api/feedback", feedbackRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
