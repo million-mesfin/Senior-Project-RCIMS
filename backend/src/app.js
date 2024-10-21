5ce225a50aff82c52871411f63daf076ce4925ac
Million Mesfin <millionmesfind@gmail.com>
2024-10-21 08:20:51 -0300

    Add progress management route to the server configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,7 @@ const {
 } = require("./controller/AppointmentController");
 const appointmentManagementRoute = require("./routes/AppointmentManagement");
 const feedbackRoute = require("./routes/Feedback");
+const progressManagementRoute = require("./routes/ProgressManagement");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -78,6 +79,7 @@ app.use("/api/patient-history", patientHistoryManagementRoute);
 app.use("/api/schedule", scheduleRoute);
 app.use("/api/appointment", appointmentManagementRoute);
 app.use("/api/feedback", feedbackRoute);
+app.use("/api/progress", progressManagementRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
